b48386a5b4602af4c8e7ae5bdbb33b16b9c3b176
yurilsn <luizyuri57@gmail.com>
2025-06-05 22:40:34 -0300

    feat: implementando enums

--- backend/src/main/java/com/acadmap/model/enums/StatusVeiculo.java
+++ backend/src/main/java/com/acadmap/model/enums/StatusVeiculo.java
@@ -1,5 +1,37 @@
 package com.acadmap.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum StatusVeiculo {
-    PENDENTE, NEGADO, ACEITO, EXCLUIDO
+
+    pendente("pendente"),
+    aceito("aceito"),
+    excluido("excluido"),
+    negado("negado");
+
+    private String codigo;
+
+    private StatusVeiculo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+
+    @JsonCreator
+    public static StatusVeiculo doValor(String codigo){
+        for(StatusVeiculo statusVeiculo : StatusVeiculo.values()){
+            if(negado.getCodigo().equals(codigo.toLowerCase())){
+                return negado;
+            }
+        }
+        throw new IllegalArgumentException("Ta errado o enum paizão");
+    }
 }
--- backend/src/main/java/com/acadmap/model/enums/TipoPerfilUsuario.java
+++ backend/src/main/java/com/acadmap/model/enums/TipoPerfilUsuario.java
@@ -1,7 +1,37 @@
 package com.acadmap.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TipoPerfilUsuario {
-    PESQUISADOR,
-    AUDITOR,
-    ADMINISTRADOR
+
+    administrador("administrador"),
+    auditor("auditor"),
+    pesquisador("pesquisador");
+
+
+    private String codigo;
+
+    private TipoPerfilUsuario(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+
+    @JsonCreator
+    public static TipoPerfilUsuario doValor(String codigo){
+        for(TipoPerfilUsuario tipoPerfilUsuario : TipoPerfilUsuario.values()){
+            if(tipoPerfilUsuario.getCodigo().equals(codigo.toUpperCase())){
+                return tipoPerfilUsuario;
+            }
+        }
+        throw new IllegalArgumentException("Ta errado o enum paizão");
+    }
 }
