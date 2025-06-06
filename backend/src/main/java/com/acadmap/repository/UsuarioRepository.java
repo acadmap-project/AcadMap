c725aa36ef929a6f9a45280dd5ab9eb1b4ae6a8d
Antonio Henrique <ahenrique.amatos@gmail.com>
2025-06-05 22:13:59 -0300

    feat(repository): add repositories for AreaPesquisa, Usuario and Programa entities

---
+++ backend/src/main/java/com/acadmap/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.acadmap.repository;
+
+import com.acadmap.model.Usuario;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+}
