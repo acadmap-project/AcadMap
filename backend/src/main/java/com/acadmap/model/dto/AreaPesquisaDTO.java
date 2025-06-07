d3116f21b519072abd3674f432af2f846a7c6f5a
Antonio Henrique <ahenrique.amatos@gmail.com>
2025-06-07 14:51:59 -0300

    feat(controller): adiciona endpoints de listagem para Programa e Área de Pesquisa

--- backend/src/main/java/com/acadmap/model/dto/AreaPesquisaDTO.java
+++ backend/src/main/java/com/acadmap/model/dto/AreaPesquisaDTO.java
@@ -1,7 +1,10 @@
 package com.acadmap.model.dto;
 
 import java.util.UUID;
+import com.acadmap.model.entities.AreaPesquisa;
 
 public record AreaPesquisaDTO(UUID id, String nome) {
-
+  public AreaPesquisaDTO(AreaPesquisa areaPesquisa) {
+    this(areaPesquisa.getIdAreaPesquisa(), areaPesquisa.getNome());
+  }
 }
--- backend/src/main/java/com/acadmap/model/dto/ProgramaDTO.java
+++ backend/src/main/java/com/acadmap/model/dto/ProgramaDTO.java
@@ -1,7 +1,10 @@
 package com.acadmap.model.dto;
 
+import com.acadmap.model.entities.Programa;
 import java.util.UUID;
 
 public record ProgramaDTO(UUID id, String nome) {
-
+  public ProgramaDTO(Programa programa) {
+    this(programa.getIdPrograma(), programa.getNome());
+  }
 }
