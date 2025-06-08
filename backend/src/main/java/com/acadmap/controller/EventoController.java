c0a83deccdc76b8ebdb3ec90e6bfcd7fc0b79308
yurilsn <luizyuri57@gmail.com>
2025-06-08 19:47:13 -0300

    fix: service de cadastro de eventos

--- backend/src/main/java/com/acadmap/controller/EventoController.java
+++ backend/src/main/java/com/acadmap/controller/EventoController.java
@@ -2,12 +2,15 @@ package com.acadmap.controller;
 
 import com.acadmap.model.dto.EventoCreateDTO;
 import com.acadmap.model.entities.Evento;
+import com.acadmap.model.entities.Usuario;
 import com.acadmap.service.EventoService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/eventos/cadastro")
@@ -17,9 +20,9 @@ public class EventoController {
 
   private final EventoService eventoService;
 
-  @PostMapping
-  public ResponseEntity<Evento> criarEvento(@RequestBody EventoCreateDTO dto) {
-    Evento evento = this.eventoService.criarEvento(dto);
+  @PostMapping("/{id}")
+  public ResponseEntity<Evento> criarEvento(@RequestBody EventoCreateDTO dto, @PathVariable("id") UUID uuid) {
+    Evento evento = this.eventoService.criarEvento(dto, uuid);
     return ResponseEntity.status(HttpStatus.CREATED).body(evento);
   }
 }
